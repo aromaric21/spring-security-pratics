fd380cbdfb657a1ce0b7345a7dfd59c14871efcd
aromaric <aromaricadjei213@gmail.com>
2025-06-24 09:13:13 +0000

    Spring security config in memory user details with annotation sec config

--- src/main/java/com/aromaric/sec/SecurityConfig.java
+++ src/main/java/com/aromaric/sec/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.aromaric.sec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
@@ -39,8 +41,8 @@ public class SecurityConfig {
         return http
                 .formLogin(fl->fl.loginPage("/login").permitAll())
                 .csrf(Customizer.withDefaults())
-                .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
-                .authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
+                //.authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
+                //.authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
                 .authorizeHttpRequests(ar->ar.requestMatchers("/public/**", "/webjars/**").permitAll())
                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
                 .exceptionHandling(eh->eh.accessDeniedPage("/notAuthorized"))
--- src/main/java/com/aromaric/web/ProductController.java
+++ src/main/java/com/aromaric/web/ProductController.java
@@ -5,6 +5,7 @@ import com.aromaric.repository.ProductRepository;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -25,6 +26,7 @@ public class ProductController {
     }
 
     @GetMapping("/user/index")
+    @PreAuthorize("hasRole('USER')")
     public String index(Model model){
         List<Product> products = productRepository.findAll();
         model.addAttribute("productList", products);
@@ -37,18 +39,21 @@ public class ProductController {
     }
 
     @PostMapping("/admin/delete")
+    @PreAuthorize("hasRole('ADMIN')")
     public String delete(@RequestParam(name = "id") Long id){
         productRepository.deleteById(id);
         return "redirect:/user/index";
     }
 
     @GetMapping("/admin/newProduct")
+    @PreAuthorize("hasRole('ADMIN')")
     public String newProduct(Model model){
         model.addAttribute("product", new Product());
         return "new-product";
     }
 
     @PostMapping("/admin/saveProduct")
+    @PreAuthorize("hasRole('ADMIN')")
     public String saveProduct(@Valid Product product
             , BindingResult bindingResult
              , Model model){
